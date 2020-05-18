c30d628b94153881de4b92e7ca5adabaef772dde
Bobby Neal <rsneal01@gmail.com>
2020-05-18 16:27:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
   
   def add_student(name, grade)
     @roster[grade] = []
-    if @roster[grade]. == 0
+    if @roster[grade] == nil
       
       @roster[grade] << name
     else 
