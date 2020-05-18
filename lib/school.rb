e45ff2910107085314c38d8d2b3d1c3855b901e8
Bobby Neal <rsneal01@gmail.com>
2020-05-18 16:39:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@
 
 class School
   
-  attr_accessor :roster, :school
+  attr_accessor :roster, :school, :grade
   
   def initialize(school)
     @school = school
@@ -20,7 +20,7 @@ class School
 end
 
   def grade(grade)
-    @school[grade]
+    @roster[grade]
   end
   
   
