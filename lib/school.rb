15e6a26215d641f1b5c06ddbef28d07810623543
Bobby Neal <rsneal01@gmail.com>
2020-05-18 16:02:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster, :school
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster[grade] = []
+    else @roster[grade] << name
+    end
+  end
+end
